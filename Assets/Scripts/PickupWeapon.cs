465d4a48c14451bb0c4723e5c20fd394f57db4b9
layanalsada7 <layanalsada7@gmail.com>
2023-12-07 03:42:31 +0000

    :)

--- Assets/Scripts/PickupWeapon.cs
+++ Assets/Scripts/PickupWeapon.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pickup : MonoBehaviour
+public class PickupWeapon : MonoBehaviour
 {
     private Inventory inventory;
     public GameObject itemButton;
