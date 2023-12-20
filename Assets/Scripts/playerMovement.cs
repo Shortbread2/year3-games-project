bbcb18aba5d282984ff3daaa206d317f31a7efc3
jordanBurns <117133322+Shortbread2@users.noreply.github.com>
2023-12-20 13:57:21 +0000

    sorted merged issue from prev commit
    may break everything, so zip everything first

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,58 +4,43 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    //this script is attached to the player object in the scene
+    //basic controls of moving up, down, left and right
     public float moveSpeed = 1f; // player movement speed
     public float runSpeed = 2f; // player run speed
     public Rigidbody2D rb; // reference to Rigidbody2D component
     public Animator animator; // reference to Animator component
     public bool isScriptActive;
-
-<<<<<<< Updated upstream
-    Vector2 movement; // store movement input
-=======
-    public float moveSpeed = 1f; //player movement speed
-    public float sprintMultiplier = 1.4f;
-    private float sprintModifier = 1f;
-    public Rigidbody2D rb; //reference to Rigidbody2D component 
-    public Animator animator; //reference to Animator component
     Vector2 movement; //store movement input
->>>>>>> Stashed changes
 
     void Update()
 
     {
+        movement.x = Input.GetAxisRaw("Horizontal"); //get input for horizontal movement axis
+        movement.y = Input.GetAxisRaw("Vertical"); //get input for vertial movement axis
         if (isScriptActive)
         {
             movement.x = Input.GetAxisRaw("Horizontal"); // get input for horizontal movement axis
             movement.y = Input.GetAxisRaw("Vertical"); // get input for vertical movement axis
 
+        movement.Normalize();
             movement.Normalize();
-
-<<<<<<< Updated upstream
-            // Set animator parameters based on input
-            animator.SetFloat("horizontal", movement.x);
-            animator.SetFloat("vertical", movement.y);
-            animator.SetFloat("speed", movement.magnitude);
-        }
-=======
-        if (Input.GetKey(KeyCode.LeftShift)){
-            sprintModifier = sprintMultiplier;
-        } else{
-             sprintModifier = 1f;
-        }
 
         //Set animator parameters based on input
         animator.SetFloat("horizontal", movement.x);
         animator.SetFloat("vertical", movement.y);
         animator.SetFloat("speed", movement.magnitude);
-        animator.SetFloat("MoveSpeed", sprintModifier);
-
->>>>>>> Stashed changes
+            // Set animator parameters based on input
+            animator.SetFloat("horizontal", movement.x);
+            animator.SetFloat("vertical", movement.y);
+            animator.SetFloat("speed", movement.magnitude);
+        }
     }
 
     void FixedUpdate()
     {
-<<<<<<< Updated upstream
+        //move player object position
+        rb.AddForce(movement * moveSpeed * Time.fixedDeltaTime);
         if (isScriptActive)
         {
             // Move player object position
@@ -68,9 +53,6 @@ public class playerMovement : MonoBehaviour
                 rb.AddForce(movement * moveSpeed * Time.fixedDeltaTime);
             }
         }
-=======
-        //move player object position
-        rb.AddForce(movement * moveSpeed * sprintModifier * Time.fixedDeltaTime);
->>>>>>> Stashed changes
     }
+
 }
