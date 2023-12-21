ec9fc73564154cbdbcce26b193264d2941c77412
layanalsada7 <layanalsada7@gmail.com>
2023-12-21 18:27:47 +0000

    why did it just get fixed

--- Assets/Scripts/CutScenes.cs
+++ Assets/Scripts/CutScenes.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class CutScenes : MonoBehaviour
 {
 
-    [SerializeField] GameObject? Inventory;
+    [SerializeField] GameObject Inventory;
     void Start()
     {
         // Freeze time
