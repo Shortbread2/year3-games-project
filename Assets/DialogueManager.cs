e0b75e3ea1f18852915a7fab8ac3f0afda89a4e3
layanalsada7 <layanalsada7@gmail.com>
2023-12-22 03:55:01 +0000

    pls dont break

---
+++ Assets/DialogueManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI nameText; 
+    public TextMeshProUGUI dialogueText;
+    public GameObject dialogueBox;
+
+    private Queue<string> sentences;
+    public bool isDialogueEnded = false;
+
+    private void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        isDialogueEnded = false;
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+
+    public void EndDialogue()
+    {
+        Debug.Log("End of conversation");
+        dialogueBox.SetActive(false);
+        isDialogueEnded = true;
+    }
+}
