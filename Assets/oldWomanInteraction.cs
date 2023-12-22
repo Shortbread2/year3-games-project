947a355a60af95b2f9881c5fce5f2912c82aef81
layanalsada7 <layanalsada7@gmail.com>
2023-12-22 00:36:39 +0000

    woooooo

---
+++ Assets/oldWomanInteraction.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OldWomanInteraction : MonoBehaviour
+{
+    public void SpawnEnemies()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if(enemies.Length < 0 )
+        {
+            Debug.Log("No enemy tags found");
+        }
+
+        Debug.Log(enemies.Length);
+
+        foreach (GameObject enemy in enemies)
+        {
+            enemy.SetActive(true);
+        }
+    }
+}
